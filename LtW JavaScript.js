5e799be61e659e16373028a225934bb4b82da84f
elusch21 <eschreiber@eastsideprep.org>
2015-01-16 10:25:20 -0800

    added my code

--- LtW JavaScript.js
+++ LtW JavaScript.js
@@ -1,44 +1,5 @@
 var cash = 1000;
 var turns = 0;
-
-function addTurns(turns) {
-	var adding = prompt("Would you like to add a turn?");
-	switch(adding.toLowerCase()) {
-		case("yes") :
-			turns++;
-			break;
-		default :
-			break;
-	};
-	var hack = prompt("Do you want to input a code?");
-	switch (hack) {
-		case("9001"):
-			units++;
-			createGoku();
-			break;
-		case("S-117"):
-			units++;
-			createJohn();
-			break;
-		case("Dovahkin"):
-			units++;
-			createDragonborn();
-			break;
-		default:
-			break;
-	}
-	//I need to add createGoku(), createJohn(), and createDragonborn().
-}
-
-
-
-function addMoney() {
-	if (turns % 2 === 0) {
-		cash += 100;
-		break;
-	}
-}
-
 var units = 0;
 
 function Vehicle(cost, attack, hitPoints, speed, range, accuracy) {
@@ -61,22 +22,22 @@ function Vehicle(cost, attack, hitPoints, speed, range, accuracy) {
 	units++
 }
 
-	function Motorbike() {
+	function Motorbike(name) {
 		this.name = name;
 	}
 	Motorbike.prototype = new Vehicle(75, 12, 8, 14, 10, 10);
 
-	function ArmoredCar() {
+	function ArmoredCar(name) {
 		this.name = name;
 	}
 	ArmoredCar.prototype = new Vehicle(125, 12, 20, 14, 6, 6);
 
-	function ArmoredTruck() {
+	function ArmoredTruck(name) {
 		this.name = name;
 	}
 	ArmoredTruck.prototype = new Vehicle(125, 12, 24, 10, 6, 6);
 
-	function Tank() {
+	function Tank(name) {
 		this.name = name;
 	}
 	Tank.prototype = new Vehicle(200, 20, 15, 5, 10, 10);
@@ -126,3 +87,65 @@ function Special(cost, range) {
 		cash += 500;
 	}
 	CashExtractor.prototype = new Special(150, 0);
+function Infantry(cost, level, attack, defense, speed, range, accuracy) {
+		this.cost = cost;
+		this.level = level;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+		this.range = range;
+		this.accuracy = accuracy;
+		Infantry.instances++;
+	}
+	Infantry.instances = 0;
+	function Rifleman(name) {
+		this.name = name;
+	} 
+	Rifleman.prototype = new Infantry(25, 1, 10, 10, 10, 10, 10);
+	function Heavy(name) {
+		this.name = name;
+	}
+	Heavy.prototype = new Infantry(50, 1, 12, 15, 7, 10, 8);
+	function Commander(name) {
+		this.name = name;
+	}
+	Commander.prototype = new Infantry(75, 1, 6, 6, 15, 12, 15);
+	function Flamethrower(name) {
+		this.name = name;
+	} 
+	Flamethrower.prototype = new Infantry(75, 1, 20, 10, 9, 5, 10);
+	function Sniper(name) {
+		this.name = name;
+	} 
+	Sniper.prototype = new Infantry(100, 1, 15, 4, 7, 15, 15);
+	function Mortar(name) {
+		this.name = name;
+	} 
+	Mortar.prototype = new Infantry(100, 1, 15, 15, 0, 17, 9);
+	function Aircraft(cost, level, attack, defense, speed, range, accuracy) {
+		this.cost = cost;
+		this.level = level;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+		this.range = range;
+		this.accuracy = accuracy;
+		Aircraft.instances++;
+	}
+	Aircraft.instances = 0;
+	function Paratrooper(name) {
+		this.name = name;
+	}
+	Paratrooper.prototype = new Aircraft(75, 1, 12, 8, 14, 12, 8);
+	function Helicopter(name) {
+		this.name = name;
+	}
+	Helicopter.prototype = new Aircraft(150, 1, 12, 12, 14, 12, 10);
+	function Airplane(name) {
+		this.name = name;
+	}
+	Airplane.prototype = new Aircraft(175, 1, 16, 12, 12, 12, 10);
+	function Bomber(name) {
+		this.name = name;
+	}
+	Bomber.prototype = new Aircraft(200, 1, 20, 10, 12, 12, 10);
